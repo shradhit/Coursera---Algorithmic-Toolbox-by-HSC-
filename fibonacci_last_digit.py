73506859fede53b4df53a069dd49966eee2df472
Shradhit Subudhi <shradhit@gmail.com>
2020-02-17 17:25:46 -0500

    Update fibonacci_last_digit.py

--- fibonacci_last_digit.py
+++ fibonacci_last_digit.py
@@ -1,12 +1,17 @@
 # Uses python3
+
+"""
+ -  The last digit of Fibonacci follows a pattern! I found the pattern and then applied it to this code. 
+ 
+"""
 import sys
 
 def get_fibonacci_last_digit_naive(n):
 
-    fix =  [0, 1, 1, 2, 3,5, 8, 3,1,4,5,9,4, 3,7,0,7,7,4,1,5,6,1,7,8,5,3,8,1,9,9,0,9,8,7,5,2,7,9,6,5,1,6,7,3,0,3,3,6,9,5,4,9,3,2,5,7,2,9,1]
+    pattern =  [0, 1, 1, 2, 3,5, 8, 3,1,4,5,9,4, 3,7,0,7,7,4,1,5,6,1,7,8,5,3,8,1,9,9,0,9,8,7,5,2,7,9,6,5,1,6,7,3,0,3,3,6,9,5,4,9,3,2,5,7,2,9,1]
     y = n % 60
 
-    return fix[y]
+    return pattern[y]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
