fc663e9a2e9bf0e46e4ed4e37c486b23de8a2dac
Shradhit Subudhi <shradhit@gmail.com>
2020-02-18 14:01:11 -0500

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -3,7 +3,13 @@ def calc_fib(n):
     if (n <= 1):
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    a = 0
+    b = 1
+    for _ in range(n-1):
+        c = a + b
+        a, b = b, c
+
+    return c
 
 n = int(input())
 print(calc_fib(n))
