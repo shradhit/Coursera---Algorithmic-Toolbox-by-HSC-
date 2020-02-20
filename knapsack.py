9edc9f00c967c7ac62f80802450ca1c2b4a99aa5
Shradhit Subudhi <shradhit@gmail.com>
2020-02-20 15:16:10 -0500

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -8,16 +8,15 @@ def get_optimal_value(capacity, weights, values):
     # write your code here
     value_perunit_weight = [x/y for x, y in zip(values, weights)]
 
-    #print(value_perunit_weight)
 
     while capacity and len(value_perunit_weight) != 0:
-        #print(max(value_perunit_weight))
+
         max_value_rn = max(value_perunit_weight)
         max_value_index = value_perunit_weight.index(max_value_rn)
         value_perunit_weight.remove(max_value_rn)
 
         check = capacity - weights[max_value_index]
-        #print(check)
+        
         if check > 0 :
             value += values[max_value_index]
             capacity = capacity - weights[max_value_index]
@@ -32,9 +31,7 @@ if __name__ == "__main__":
     data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
-    #print(values)
     weights = data[3:(2 * n + 2):2]
-    #print(weights)
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
 
