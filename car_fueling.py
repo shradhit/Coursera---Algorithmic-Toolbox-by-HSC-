28844ba45bfd36936a5e75fae55f2f8af9f9ba4b
Shradhit Subudhi <shradhit@gmail.com>
2020-02-21 10:57:10 -0500

    Update car_fueling.py

--- car_fueling.py
+++ car_fueling.py
@@ -5,7 +5,7 @@ def compute_min_refills(distance, tank, stops):
     number_stops = 0
     distance_travel = 0
     stops.append(distance)
-    #[y-x for x,y in pairwise(a)]
+    
     difference = [t - s for s, t in zip(stops, stops[1:])]
 
     if any(i > tank for i in difference):
