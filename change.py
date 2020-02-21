89140011bc16b434ca1918619f6ab06a9b632ea6
Shradhit Subudhi <shradhit@gmail.com>
2020-02-21 11:30:13 -0500

    Update change.py

--- change.py
+++ change.py
@@ -1,11 +1,15 @@
 # Uses python3
+"""
+You have three coins 10, 5, 1. Minimum number of coins required to make up the number 'm'.
+"""
+
 import sys
 
 def get_change(m):
     x = y = x1 = y1 = 0
-    if m > 10:
-        x = m / 10
-        y = m % 10
+    
+    x = m / 10
+    y = m % 10
     x1 = y / 5
     y1 = y % 5
     ans = int(x) + int(x1) + y1
